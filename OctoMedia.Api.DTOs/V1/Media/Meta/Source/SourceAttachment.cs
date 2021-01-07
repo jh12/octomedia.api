dd46fe4c83adb6740b1f45cef27483156f3e7060
jh12 <jabaha35@hotmail.com>
2021-01-07 16:47:57 +0100

    fix: rename SourceAttachments and remove key

---
+++ OctoMedia.Api.DTOs/V1/Media/Meta/Source/SourceAttachment.cs
@@ -0,0 +1,16 @@
+﻿namespace OctoMedia.Api.DTOs.V1.Media.Meta.Source
+{
+    public class SourceAttachment
+    {
+        public RedditSource? Reddit { get; set; }
+
+        public SourceAttachment()
+        {
+        }
+
+        public SourceAttachment(RedditSource? reddit)
+        {
+            Reddit = reddit;
+        }
+    }
+}
