da037923c08318d6c23c28ecd8c13830c794ff25
jh12 <jabaha35@hotmail.com>
2021-05-23 15:29:36 +0200

    fix: seed mongodb collections

--- OctoMedia.Api.DataAccess.MongoDB/Factories/MongoDBContextFactory.cs
+++ OctoMedia.Api.DataAccess.MongoDB/Factories/MongoDBContextFactory.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System.Collections.Generic;
+using Autofac;
+using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 using OctoMedia.Api.Common.Options;
+using OctoMedia.Api.DataAccess.MongoDB.Models;
 
 namespace OctoMedia.Api.DataAccess.MongoDB.Factories
 {
-    public class MongoDBContextFactory
+    public class MongoDBContextFactory : IStartable
     {
         private readonly MongoDBOptions _mongoDbOptions;
 
@@ -15,13 +18,7 @@ namespace OctoMedia.Api.DataAccess.MongoDB.Factories
 
         public IMongoDatabase GetDatabase()
         {
-            MongoClient client = new MongoClient(new MongoClientSettings
-            {
-                Server = MongoServerAddress.Parse(_mongoDbOptions.Server),
-                Credential = MongoCredential.CreateCredential(_mongoDbOptions.Database, _mongoDbOptions.Username, _mongoDbOptions.Password)
-            });
-
-            return client.GetDatabase(_mongoDbOptions.Database);
+            return GetClient().GetDatabase(_mongoDbOptions.Database);
         }
 
         public IMongoCollection<T> GetContext<T>(string collectionName)
@@ -29,6 +26,65 @@ namespace OctoMedia.Api.DataAccess.MongoDB.Factories
             IMongoDatabase database = GetDatabase();
 
             return database.GetCollection<T>(collectionName);
+        }
+
+        private MongoClient GetClient()
+        {
+            return new MongoClient(new MongoClientSettings
+            {
+                Server = MongoServerAddress.Parse(_mongoDbOptions.Server),
+                Credential = MongoCredential.CreateCredential(_mongoDbOptions.Database, _mongoDbOptions.Username, _mongoDbOptions.Password)
+            });
+        }
+
+        public void Start()
+        {
+            SeedDatabase();
+        }
+
+        private static object _lock = new();
+        private static bool _isInitialized;
+
+        private void SeedDatabase()
+        {
+            lock (_lock)
+            {
+                if (_isInitialized)
+                    return;
+
+                MongoClient client = GetClient();
+
+                IMongoDatabase mongoDatabase = client.GetDatabase(_mongoDbOptions.Database);
+
+                List<string> collectionNames = mongoDatabase.ListCollectionNames().ToList();
+
+                // Media
+                {
+                    if (!collectionNames.Contains(MongoDBCollectionNames.MediaCollection))
+                        mongoDatabase.CreateCollection(MongoDBCollectionNames.MediaCollection);
+
+                    IndexKeysDefinition<MongoMedia> sourceIdDefinition = Builders<MongoMedia>.IndexKeys.Ascending(s => s.SourceId);
+                    mongoDatabase.GetCollection<MongoMedia>(MongoDBCollectionNames.MediaCollection).Indexes.CreateOne(new CreateIndexModel<MongoMedia>(sourceIdDefinition));
+                }
+
+                // Source
+                {
+                    if (!collectionNames.Contains(MongoDBCollectionNames.SourceCollection))
+                        mongoDatabase.CreateCollection(MongoDBCollectionNames.SourceCollection);
+
+                    IndexKeysDefinition<MongoSource> siteUriDefinition = Builders<MongoSource>.IndexKeys.Hashed(s => s.SiteUri);
+
+                    mongoDatabase.GetCollection<MongoSource>(MongoDBCollectionNames.SourceCollection).Indexes.CreateOne(new CreateIndexModel<MongoSource>(siteUriDefinition));
+                }
+
+                // Counters int
+                {
+                    if (!collectionNames.Contains(MongoDBCollectionNames.IntCounterCollection))
+                        mongoDatabase.CreateCollection(MongoDBCollectionNames.IntCounterCollection);
+                }
+
+                _isInitialized = true;
+            }
         }
     }
 }
